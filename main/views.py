411b1d3be07fa9e5f1ceb408e5ad5aa3523d1484
VeskoSpasovTUES <127239824+VeskoSpasovTUES@users.noreply.github.com>
2023-03-11 03:17:08 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,37 +1,48 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+
 from .models import *
-from .forms import CreateNewPassword, Register
+from .forms import CreateNewPassword, Register, TwoFactorAuthentication
+from .decorations import *
+from main.helper_functions.password_hash import encrypt, decrypt
 
 
 # Create your views here.
+@login_required(login_url="login")
 def password_list(request):
     passwords = request.user.passwords_set.filter(user_id=request.user.id)
+    for password in passwords:
+        password.website_password = decrypt(password.website_password)
     return render(request, "main/list.html", {"passwords": passwords})
 
 
+@login_required(login_url="login")
 def add_password(request):
     if request.method == "POST":
         form = CreateNewPassword(request.POST)
         if form.is_valid():
             n = form.cleaned_data["website"]
             p = form.cleaned_data["website_password"]
+            p = encrypt(p)
             request.user.passwords_set.create(website=n, website_password=p)
             return HttpResponseRedirect("/passwords")
         else:
             print(form.errors)
     else:
         form = CreateNewPassword()
-    return render(request, "main/add_password.html", {"form": form})
+    return render(request, "main/add_password.html", {"form": form}, {})
 
 
+@login_required(login_url="login")
 def home(response):
     return render(response, "main/home.html", {})
 
 
+@unauthenticated_user
 def register(request):
     form = Register()
     if request.method == "POST":
@@ -45,6 +56,7 @@ def register(request):
     return render(request, "main/register.html", {"form": form})
 
 
+@unauthenticated_user
 def login_request(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -63,6 +75,7 @@ def login_request(request):
     return render(request, "main/login.html", {"form": form})
 
 
+@login_required(login_url="login")
 def logout_request(request):
     logout(request)
     return redirect("home")
